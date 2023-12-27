85e481c2e390d6c8bfc7c59cbbd35941cf2fb5e4
mogzame <moxamemoxame@gmail.com>
2023-12-27 20:43:21 +0100

    Add custom grid button and function.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const btn = document.querySelector('.custom');
 
 //create a box 256 times (for loop)
 //with a height and width of 25px
@@ -15,4 +16,26 @@ for (let i = 0; i < 256; i++) {
 
 container.addEventListener('mouseover', (e) => {
     e.target.style.backgroundColor = 'black';
+})
+
+//when btn is clicked => prompt num of grid each side and store it in var
+//new var squaresnumber is num of grids * itself
+//new var heigh and width 400/num of grids
+//create a loop where i < squaresnumber
+//setatt of style with height = height and width = width
+//append it to the container
+
+btn.addEventListener('click', () => {
+    let gridNum = prompt('Number of squares per side for the grid');
+    console.log(gridNum)
+    let squaresNum = gridNum ** 2;
+    let squaresDimension = 400 / gridNum;
+    console.log(squaresDimension)
+
+    for (i = 0; i < squaresNum; i++) {
+        let square = document.createElement('div');
+        square.style.width = `${squaresDimension}px`;
+        square.style.height = `${squaresDimension}px`;
+        container.append(square);
+    }
 })
